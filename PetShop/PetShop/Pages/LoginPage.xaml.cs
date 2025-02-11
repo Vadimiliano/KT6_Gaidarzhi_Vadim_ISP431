410d9ce3b4395266dd20fa0814de76f9a79d5e00
Коммерческие курсы <comcuru@teach.local>
2025-02-11 12:34:30 +0300

    Work authorization and global style

---
+++ PetShop/PetShop/Pages/LoginPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PetShop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StringBuilder errors = new StringBuilder();
+
+                if (string.IsNullOrEmpty(UserLoginTextBox.Text))
+                {
+                    errors.AppendLine("Введите логин");
+                }
+                if (string.IsNullOrEmpty(UserPasswordTextBox.Password))
+                {
+                    errors.AppendLine("Введите пароль");
+                }
+
+                if(errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString(), "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                if(Data.TradeEntities.GetContext().User.Any(d => d.UserLogin == UserLoginTextBox.Text && d.UserPassword == UserPasswordTextBox.Password))
+                {
+                    MessageBox.Show("Успех!", "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Classes.Manager.MainFrame.Navigate(new Pages.ProductPage());
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ошибка!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+        }
+    }
+}
